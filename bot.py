11c7e32d0b741d44322d414a458fe975dd435002
UnderSaga <undersaga@mail.ru>
2026-01-14 14:46:43 +0300

    New greeting text

--- bot.py
+++ bot.py
@@ -4,11 +4,12 @@ from telebot import types
 bot = telebot.TeleBot("8002325739:AAGeU3IRe9CKdoHiok9aH0KtnU_aYkTd2lQ", parse_mode=None)
 
 rules = """
-Приветствуем тебя в нашей беседе!
-Чтобы продолжить общение, комфортное для всех - ознакомься с правилами:
-1. Не пиши;
-2. Не дыши;
-3. Бан тебе!
+Приветствуем тебя в нашей таверне! Заходи располагайся. 
+Дабы всём тут было комфортно, соблюдай некоторые правила! 
+1. Вся ругань в личные сообщения. 
+2. Реклама без согласования со мной запрещена. 
+3. Обсуждение религии и политики запрещено. 
+Веди себя хорошо!)
 """
 
 @bot.message_handler(content_types=['new_chat_members'])  
@@ -19,7 +20,7 @@ def greeting(message):
 def inviteButton(message):
     if(message.is_automatic_forward):
         markup = types.InlineKeyboardMarkup()
-        btn1 = types.InlineKeyboardButton(text='Наша лютая беседка', url='https://t.me/+kuf7BiVM1Xc1YTdi')
+        btn1 = types.InlineKeyboardButton(text='Присоединиться к Совёнку', url='https://t.me/+kuf7BiVM1Xc1YTdi')
         markup.add(btn1)
         bot.reply_to(message, "Садись за барную стойку! У Совёнка есть пару историй для тебя!", reply_markup = markup)
 
